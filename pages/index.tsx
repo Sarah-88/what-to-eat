e6cd4291bc0f40bdc94b9bca4226502f1c6a0379
Sarah-88 <sarahlim88@hotmail.com>
2023-09-14 20:06:56 +0800

    add disclaimer and link to full list

--- pages/index.tsx
+++ pages/index.tsx
@@ -93,12 +93,13 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div className="text-center p-3">
+            <main className="text-center p-3">
                 <h1 className={`text-4xl mt-5 ${carter.className} theme-color font-sans`}>What to eat today?</h1>
                 {chosenIdx > -1 &&
                     <div className="ml-auto mr-auto mt-3 flex max-w-3xl justify-center">
                         <button type="button" className="border-theme-color border p-2 rounded text-sm m-1" onClick={reset}>Suggest Others</button>
                         <button type="button" className="border-theme-color border p-2 rounded text-sm m-1" onClick={changeCat}>Change Category</button>
+                        <a href="/recipes" className="border-theme-color border p-2 rounded text-sm m-1 inline-block">View All</a>
                     </div>
                 }
                 <div className={`whitespace-nowrap transition-all duration-500 -ml-3 -mr-3 ${styles.moveCategory}`} style={{
@@ -181,7 +182,10 @@ export default function Home() {
                         <path fill="none" className="stroke-theme-color" strokeWidth={15} d="M207.9 15.2c.8 4.7 16.1 94.5 16.1 128.8 0 52.3-27.8 89.6-68.9 104.6L168 486.7c.7 13.7-10.2 25.3-24 25.3H80c-13.7 0-24.7-11.5-24-25.3l12.9-238.1C27.7 233.6 0 196.2 0 144 0 109.6 15.3 19.9 16.1 15.2 19.3-5.1 61.4-5.4 64 16.3v141.2c1.3 3.4 15.1 3.2 16 0 1.4-25.3 7.9-139.2 8-141.8 3.3-20.8 44.7-20.8 47.9 0 .2 2.7 6.6 116.5 8 141.8.9 3.2 14.8 3.4 16 0V16.3c2.6-21.6 44.8-21.4 48-1.1zm119.2 285.7l-15 185.1c-1.2 14 9.9 26 23.9 26h56c13.3 0 24-10.7 24-24V24c0-13.2-10.7-24-24-24-82.5 0-221.4 178.5-64.9 300.9z" />
                     </svg>
                 </div>
-            </div>
+                <div className="fixed bottom-0 right-0 p-1">
+                    <span className="theme-color text-xs italic">recipes from rasamalaysia.com</span>
+                </div>
+            </main>
         </>
     )
 }
